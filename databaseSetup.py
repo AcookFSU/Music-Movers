8a87e4ac5c0fd233a5b0d6c8026cdccbbaad4605
florah12 <fac20b@fsu.edu>
2024-06-11 03:35:29 -0400

    Update databaseSetup.py
    added roles

--- databaseSetup.py
+++ databaseSetup.py
@@ -23,4 +23,17 @@ mycursor.execute("CREATE TABLE IF NOT EXISTS users (userId INT AUTO_INCREMENT PR
 mycursor.execute("CREATE TABLE IF NOT EXISTS songs (songId INT AUTO_INCREMENT PRIMARY KEY, name TEXT, artistUserId INT , lyrics LONGTEXT)")
 mycursor.execute("CREATE TABLE IF NOT EXISTS posts (postId INT AUTO_INCREMENT PRIMARY KEY, songId INT NOT NULL, authorUserId INT NOT NULL, interp TEXT, postScore INT)")
 mydb.commit()
+
+mycursor.execute("CREATE ROLE IF NOT EXISTS admin")
+mycursor.execute("CREATE ROLE IF NOT EXISTS editor")
+mycursor.execute("CREATE ROLE IF NOT EXISTS viewer")
+
+mycursor.execute("GRANT ALL PRIVILEGES ON musicMovers.* TO 'admin'")
+mycursor.execute("GRANT SELECT, INSERT, UPDATE ON musicMovers.* TO 'editor'")
+mycursor.execute("GRANT SELECT ON musicMovers.* TO 'viewer'")
+
+mycursor.execute("FLUSH PRIVILEGES")
+
+mydb.commit()
 print("Succesfully setup music movers!")
+
