39109c3731203666d22c3caf0ff850a69c655806
antoniollanes <antoniollanes128@gmail.com>
2024-06-18 20:25:44 -0400

    added logo to signup page, along with site header with back to home button, and a few decorative changes

--- createTestRecords.py
+++ createTestRecords.py
@@ -12,11 +12,11 @@ mycursor = mydb.cursor()
 mycursor.execute("USE musicMovers")
 # Insert records into the users table
 mycursor.execute("INSERT INTO users (username, password, joinDate, userType, userScore) VALUES ('alice', 'e99a18c428cb38d5f260853678922e03', '2024-01-01', 'listener', 100)")
-mycursor.execute("INSERT INTO users (username, password, joinDate, userType, userScore) VALUES ('bob', 'ab56b4d92b40713acc5af89985d4b786', '2024-01-05', 'artist', 200)")
+mycursor.execute("INSERT INTO users (username, password, joinDate, userType, userScore) VALUES ('Spongebob', 'ab56b4d92b40713acc5af89985d4b786', '2024-01-05', 'artist', 200)")
 mycursor.execute("INSERT INTO users (username, password, joinDate, userType, userScore) VALUES ('carol', '5f4dcc3b5aa765d61d8327deb882cf99', '2024-01-10', 'moderator', 300)")
 
 # Insert records into the songs table
-mycursor.execute("INSERT INTO songs (name, artistUserId, lyrics) VALUES ('Song One', 2, 'These are the lyrics of song one.')")
+mycursor.execute("INSERT INTO songs (name, artistUserId, lyrics) VALUES ('Campfire Song', 2, 'Intro SpongeBob<br>I call this one the Campfire Song Song<br>Lets gather around the campfire<br>And sing our campfire song<br>Our CAMPFIRE SONG song<br>And if you dont think that we can sing it faster then youre wrong<br>But itll help if you just sing along<br><br>Bridge Patrick<br>Bom bom bom<br><br>Verse SpongeBob  Patrick<br>CAMPFIRE SONG song<br>CAMPFIRE SONG song<br>And if you dont think that we can sing it faster then youre wrong<br>But itll help if you just sing along<br>CAMPFIRE SONG song<br>Patrick<br>Song CAMPFIRE<br>Squidward<br>Good<br><br>Outro SpongeBob<br>Itll help<br>Itll help<br>If you just sing along<br>Oh yeah')")
 mycursor.execute("INSERT INTO songs (name, artistUserId, lyrics) VALUES ('Song Two', 2, 'These are the lyrics of song two.')")
 mycursor.execute("INSERT INTO songs (name, artistUserId, lyrics) VALUES ('Song Three', 2, 'These are the lyrics of song three.')")
 
